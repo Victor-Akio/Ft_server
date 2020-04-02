a15bb817ab7ed30f36516c258bf29d473cac8c30
Victor <kyo.nop@gmail.com>
2020-04-01 21:24:05 -0300

    initializer configuration

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 This project consist in discovering the "docker" technology and use it to install a complete web server. This server will run multiples services: Wordpress, phpMyAdmin, and a SQL database.
 
 Also, it is important training of script production to optimze the execution of the server.
+
+CMD build:
+$> docker build -t name .
+
+CMD run:
+$> docker run --name aname -it -p 80:80 -p 443:443 name
+
+Utils : 
+- Web server with Nginx
+- SQL Database with MariaDB
+- Database web admin with PhpMyAdmin
+- OpenSource CMS with Wordpress
+- Self-signed SSL certificate with OpenSSL
--- srcs/container_init.sh
+++ srcs/container_init.sh
@@ -1,13 +1,46 @@
 service mysql start
 
-# create html folder
-mkdir /var/www/html && touch /var/www/html/index.php
-echo "<?php phpinfo(); ?>" >> /var/www/html/index.php
+#comentarios
+/*
+* - Wget is a programm that allows you to download web data, through HTTP, HTTPS and FTP.
+* - OPENSSL is a toolkit that allows the user to generate ssl keys.
+*/
+
+# Config Access
+chown -R www-data /var/www/*
+chmod -R 755 /var/www/
+
+# create localhost folder
+mkdir /var/www/localhost && touch /var/www/localhost/index.php
+echo "<?php phpinfo(); ?>" >> /var/www/localhost/index.php
+
+# Config SSL
+mkdir /etc/nginx/SSL
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=BR/ST=Sao Paulo/L=Sao Paulo/O=localhost/OU=Development/CN=localhost" 
 
 # Config NGINX
 mv ./tmp/nginx-conf /etc/nginx/sites-available/localhost
 ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/localhost
 rm -rf /etc/nginx/sites-enabled/default
+
+# Config MySQL
+echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
+echo "udate mysql.user set plugin='mysql_native_password' where user='root';" | mysql -u root --skip-password
+echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
+
+# Config PHPadmin
+mkdir /var/www/localhost/phpmyadmin
+wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
+tar -xvf phpMyAdmin-5.0.2-all-languages.tar.gz --strip-components 1 -C /var/www/localhost/phpmyadmin
+mv ./tmp/phpmyadmin.inc.php /var/www/localhost/phpmyadmin/config.inc.php
+
+#Config wordpress
+cd /tmp/
+wget -c https://wordpress.org/latest.tar.gz
+tar -xvzf latest.tar.gz
+mv wordpress/ /var/www/localhostmv
+mv /tmp/wp-config.php /var/www/localhost/wordpress
 
 service php7.3-fpm start
 service nginx start

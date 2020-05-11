255b63024f12a3f9c1b219781255680049202362
vscabell <vscabell@student.42sp.org.br>
2020-05-11 16:54:47 +0200

    auto install wp

--- ft_server/srcs/deploy.sh
+++ ft_server/srcs/deploy.sh
@@ -4,8 +4,8 @@
 apt-get update && apt-get upgrade
 
 # install nginx / mariadb / php and extensions
-apt-get install -y nginx
-apt-get install -y mariadb-server mariadb-client
+apt-get install -y nginx && \
+apt-get install -y mariadb-server mariadb-client && \
 apt-get install -y php7.3 php7.3-fpm php7.3-mysql php-common php7.3-cli \
 php7.3-common php7.3-json php7.3-opcache php7.3-readline php7.3-mbstring \
 php7.3-xml php7.3-gd php7.3-curl
@@ -21,14 +21,12 @@ tar -xvf phpMyAdmin-5.0.2-all-languages.tar.gz
 mv phpMyAdmin-5.0.2-all-languages /var/www/localhost/phpmyadmin
 cp -pr ./config.sample.inc.php /var/www/localhost/phpmyadmin/config.inc.php
 
-# Import the create_tables.sql to create tables for phpMyAdmin.
 service mysql start
 # Add the user and grant permission to ALL databases *.*
 echo "GRANT ALL PRIVILEGES ON *.* TO 'vscabell'@'localhost' IDENTIFIED BY '1234';"| mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
 
 # install wordpress and configure
-# upload with the command wget -c https://wordpress.org/latest.tar.gz
 tar -xvf latest.tar.gz
 mv wp-config.php /wordpress/
 mv wordpress /var/www/localhost/
@@ -36,8 +34,9 @@ mv wordpress /var/www/localhost/
 # create user for database (same as wordpress wp-config.php)
 # database name | user name | password
 echo "CREATE DATABASE wordpress;" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'user'@'localhost' IDENTIFIED BY '1234';"| mysql -u root
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'vitoria'@'localhost' IDENTIFIED BY '1234';"| mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
+mysql wordpress < wordpress.sql
 
 # SSL CONFIG
 openssl req -x509 -nodes -days 365 \
